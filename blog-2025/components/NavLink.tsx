2cee0e8e8415333bfcfc6bb2b6906db09c57e2c5
Craig Summers <craigsummers128@gmail.com>
2025-07-08 12:15:49 +0100

    Create navigation component.

---
+++ blog-2025/components/NavLink.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+
+type NavLinkProps = {
+  href: string,
+  label: string,
+  pathname: string
+}
+
+export default function NavLink({ href, label, pathname }: NavLinkProps) {
+  const isActive = pathname === href || (href === '/blogs' && pathname.startsWith("/blogs/"));
+  const classes = `text-xl ${isActive ? 'font-bold underline' : ''}`;
+
+  return (
+    <Link
+      href={href}
+      className={classes}
+    >
+      {label}
+    </Link>
+  )
+}
---
+++ blog-2025/components/Navigation.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+import NavLink from "@/components/NavLink";
+
+export default function Navigation() {
+  const pathname = usePathname();
+
+  // Set active page.
+
+  return (
+    <nav className="bg-green-300 flex justify-center">
+      <div className="flex justify-around w-lg pb-4">
+        <NavLink href="/" pathname={pathname} label="Home" />
+        <NavLink href="/blogs" pathname={pathname} label="Blogs" />
+        <NavLink href="/about" pathname={pathname} label="About" />
+      </div>
+    </nav>
+  )
+}
